c86bbf1fc3628646c64133e9aaea3cb4f141dc8e
MrAntony44 <antonislekkas05@gmail.com>
2024-01-11 20:30:57 +0200

    Add pigpio library and read config file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,23 @@
 const WebSocket = require('ws');
+const Gpio = require('pigpio').Gpio;
+const fs = require('fs');
+const configFile = './config.json';
+
+
+try {
+  const data = fs.readFileSync(configFile, 'utf8');
+  const config = JSON.parse(data);
+  console.log(config);
+} catch (err) {
+  console.error('Error reading file:', err);
+}
+
+
+const motor1 = new Gpio(17, { mode: Gpio.OUTPUT });
+const motor2 = new Gpio(17, { mode: Gpio.OUTPUT });
+const motor3 = new Gpio(17, { mode: Gpio.OUTPUT });
+const motor4 = new Gpio(17, { mode: Gpio.OUTPUT });
+
 
 const wss = new WebSocket.Server({ port: 8080 });
 
